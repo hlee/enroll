0135e34ca3525b1494858989ad052b150bff8777
Ken Lu <ken@seeice.com>
2016-04-22 10:19:28 -0400

    Refs #5385 #6797 - Using the asyn bus event Fixed group ER assigning to default GA issue

--- spec/factories/broker_roles.rb
+++ spec/factories/broker_roles.rb
@@ -1,7 +1,12 @@
 FactoryGirl.define do
   factory :broker_role do
     person { FactoryGirl.create(:person) }
-    sequence(:npn) {|n| "2002345#{n}" }
+    npn do
+      Forgery('basic').text(:allow_lower   => false,
+                            :allow_upper   => false,
+                            :allow_numeric => true,
+                            :allow_special => false, :exactly => 8)
+    end
     provider_kind {"broker"}
 
     trait :with_invalid_provider_kind do
